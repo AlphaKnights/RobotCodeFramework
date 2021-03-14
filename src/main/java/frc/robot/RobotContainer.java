0486b3ed29ae33058dd9d82041bbf7cd2d571f8b
Ryan O'Donnell <Rpodonnell2012@gmail.com>
2021-03-14 16:33:47 -0700

    Added Framework Code

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,28 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import static frc.robot.Constants.ButtonID.*;
+import static frc.robot.Constants.ControlPorts.*;
+import static frc.robot.Constants.SensorID.*;
+import static frc.robot.Constants.robotMovementConstants.*;
+
+
+import javax.naming.LinkLoopException;
+
+public class RobotContainer {
+
+
+    public RobotContainer() {
+        configureButtonBindings();
+    }
+
+    private void configureButtonBindings() {
+        
+    }
+}
---
+++ src/main/java/frc/robot/classes/DriveTrain.java
@@ -0,0 +1,48 @@
+package frc.robot.classes;
+
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+import static frc.robot.Constants.robotMovementConstants.*;
+
+public class DriveTrain extends DifferentialDrive {
+
+    public DriveTrain(SpeedController leftMotor, SpeedController rightMotor) {
+        super(leftMotor, rightMotor);
+    }
+
+    double zeroAngle;
+
+    public void arcadeDriveStraight(double power, double rotation, double angle) {
+        if (Math.abs(rotation) > 0) {
+            super.arcadeDrive(power, rotation);
+            zeroAngle = 0;
+        } else {
+            if (zeroAngle == 0) {
+                zeroAngle = angle;
+            }
+            if (angle-zeroAngle > driveStraightTolerance ) {
+                super.arcadeDrive(power, -.35);
+            }
+            if (angle - zeroAngle < -driveStraightTolerance) {
+                super.arcadeDrive(power, .35);
+            }
+            if (angle - zeroAngle >= -driveStraightTolerance && angle-zeroAngle <= driveStraightTolerance) {
+                super.arcadeDrive(power, 0);
+            }
+        }
+    }
+
+
+    @Override
+    public void curvatureDrive(double power, double rotation, boolean quickTurn) {
+        super.curvatureDrive(power, rotation, quickTurn);
+        // super.curvatureDrive(0, 0, false);
+    }
+
+    @Override
+    public void tankDrive(double leftPower, double rightPower) {
+        super.tankDrive(leftPower, rightPower);
+        // super.tankDrive(0, 0);
+    }
+}
